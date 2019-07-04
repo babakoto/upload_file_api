16b5a68b04095d1b5860308b0449614e56d3ab9a
Andriamiandrisoa Tokiniaina Eddy <44688702+babakoto@users.noreply.github.com>
2019-07-04 11:34:42 +0200

    Add files via upload

---
+++ ProductController.java
@@ -0,0 +1,44 @@
+package com.raikitra.raikitra_api_rest.controller;
+
+import com.google.common.io.Files;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+@RestController
+public class ProductController {
+
+    private String renameFile(){
+
+        return null;
+    }
+
+    @Value("${dir.images}")
+    private String uploadDir;
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Object> upload(@RequestParam("file")MultipartFile file) throws IOException {
+
+        Date date = new Date();
+        long time = date.getTime();
+        File convertFile = new File(uploadDir+"\\"+time+"."+Files.getFileExtension(file.getOriginalFilename()));
+        FileOutputStream fileOutputStream = new FileOutputStream(convertFile);
+
+        fileOutputStream.write(file.getBytes());
+        fileOutputStream.close();
+        System.out.println("**************************************");
+        System.out.println(uploadDir+"\\"+file.getOriginalFilename());
+        System.out.println("**************************************");
+     return new ResponseEntity<>("File is uploaded", HttpStatus.OK);
+    }
+}
